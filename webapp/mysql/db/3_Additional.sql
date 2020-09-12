7bc0315ec3f98deed2e178e4689d6167ca7065fd
cureseven <shiningcureseven@gmail.com>
2020-09-12 14:38:57 +0900

    3,4のINDEX貼った

--- webapp/mysql/db/3_Additional.sql
+++ webapp/mysql/db/3_Additional.sql
@@ -2,3 +2,5 @@ use isuumo;
 
 alter table chair add index (stock, price, id);
 alter table estate add index (rent, id);
+alter table estate add index (door_width, door_height, popularity);
+alter table estate add index (rent, popularity, id);
