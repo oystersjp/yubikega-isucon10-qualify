12ce8857026cb98d81316122a2acc72d85e3cf6b
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2020-09-22 12:56:46 +0900

    refactor: chairとestateでまとめる

--- webapp/mysql/db/3_Additional.sql
+++ webapp/mysql/db/3_Additional.sql
@@ -1,12 +1,11 @@
 use isuumo;
 
 alter table chair add index (stock, price, id);
+alter table chair add index (price);
+
 alter table estate add index (rent, id);
 alter table estate add index (door_width, door_height, popularity);
 alter table estate add index (rent, popularity, id);
-
-alter table estate add index (rent);
-alter table chair add index (price);
-
 alter table estate add index (latitude);
 alter table estate add index (longitude);
+alter table estate add index (rent);
